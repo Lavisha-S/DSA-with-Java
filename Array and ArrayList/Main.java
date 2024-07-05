49fe7a9ddca744d50a25fe588ab9a63ea22d4b6a
Lavisha S <work.lavishaa@gmail.com>
2024-07-05 15:21:59 +0530

    commit

--- Array and ArrayList/Main.java
+++ Array and ArrayList/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
 public class Main {
+public static void main(String[] args) {
+    //program for input the array
+    Scanner in=new Scanner(System.in);
+int[] arr =new int[5];
+for(int i=0; i<arr.length ;i++){
+arr[i]=in.nextInt();
+
+}
+for(int i=0; i<arr.length ;i++){
+    System.out.print(arr[i]+" ");
     
+    }
+//enhanced for loop
+for(int num:arr){
+    System.out.print(num+" ");//for each loop
+
+}
+//by Arrays.toString(arr)
+System.out.println(Arrays.toString(arr));
+
+}
 
     
 }
