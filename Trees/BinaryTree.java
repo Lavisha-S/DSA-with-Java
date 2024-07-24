7bcbdc363a320bcea131a2340dc4a395c262c4b7
Lavisha S <work.lavishaa@gmail.com>
2024-07-24 21:19:14 +0530

    Binarytree

---
+++ Trees/BinaryTree.java
@@ -0,0 +1,26 @@
+public class BinaryTree{
+    public BinaryTree(){
+
+    }
+    private static class Node{
+        int value;
+        Node left();
+        Node right();
+        public Node(int val)
+        {
+            this.value=val;
+
+        }
+        
+    }
+private Node root;
+//insert elements
+public void populate(Scanner scanner){
+    System.out.println("Enter the root Node:");
+    int value=scanner.nextInt();
+    root=new Node(value);
+    populate(scanner,root);
+
+}
+private
+}
