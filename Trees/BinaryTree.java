b52c1d0db5982101d34540029c2a3410f50c4d16
Lavisha S <work.lavishaa@gmail.com>
2024-07-24 21:40:00 +0530

    commit

--- Trees/BinaryTree.java
+++ Trees/BinaryTree.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
+
 public class BinaryTree{
     public BinaryTree(){
 
     }
     private static class Node{
         int value;
-        Node left();
-        Node right();
+        Node left;
+        Node right;
         public Node(int val)
         {
             this.value=val;
@@ -23,6 +25,22 @@ public void populate(Scanner scanner){
 
 }
 private void populate(Scanner scanner,Node node){
-    
+    System.out.println("Do you want to enter left of "+node.value);
+    boolean left=scanner.nextBoolean();
+    if(left){
+        System.out.println("Enter the value of the left of " + node.value);
+        int value =scanner.nextInt();
+        node.left=new Node(value);
+        populate(scanner,node.left);
+
+    }
+System.out.println("Do you want to enter right of" + node.value);
+boolean right=scanner.nextBoolean();
+if(right){
+    System.out.println("Enter the value of the right of"+ node.value);
+    int value=scanner.nextInt();
+    node.right=new Node(value);
+    populate(scanner,node.right);
+}
 }
 }
