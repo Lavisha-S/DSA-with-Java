3b8a8f2948a5b90b901bf747de19a50c290c36c8
Lavisha S <work.lavishaa@gmail.com>
2024-07-25 16:32:28 +0000

    commit

---
+++ Heap/Main.java
@@ -0,0 +1,16 @@
+ 
+ public class Main {
+    public static void main(String[] args)throws Exception{
+        Heap<Integer> heap=new Heap<>();
+        heap.insert(34);
+        heap.insert(45);
+        heap.insert(22);
+        heap.insert(89);
+        heap.insert(76);
+        System.out.println(heap.remove());
+        System.out.println(heap.remove());
+        System.out.println(heap.remove());
+        
+
+    }
+}
