07f079ae26f679cbe1eed31e6c4139c82ef005ff
Lavisha S <work.lavishaa@gmail.com>
2024-07-25 07:56:45 +0000

    committed

---
+++ Trees/BST.java
@@ -0,0 +1,17 @@
+class BST{
+    public class Node{
+    private int value;
+    private int height;
+    private Node left;
+    private Node right;
+public Node (int value){
+    this.value=value;
+}
+public int getValue(){
+    return value;
+}
+    }
+private Node root;
+
+}
+
